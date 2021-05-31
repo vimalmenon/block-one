9b95948d081d283a42fbcc25e7dd6c861a8f1c6b
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2021-05-31 11:22:52 +0800

    more test cases

--- partials/AccountContainer/AccountContainer.test.tsx
+++ partials/AccountContainer/AccountContainer.test.tsx
@@ -79,4 +79,34 @@ describe('AccountContainer Testing', () => {
     )
     expect(toJson(wrapper)).toMatchSnapshot()
   })
+  test('AccountContainer : Snapshot testing Select change event', () => {
+    const onAccountSelect = jest.fn()
+    const accounts: AccountModel[] = [
+      {
+        customerId: 1234,
+        accountNumber: '12233344',
+        accountType: 'Saving Account',
+        balance: 20000,
+      },
+    ]
+    const selectedAccount: AccountModel = {
+      customerId: 1234,
+      accountNumber: '12233344',
+      accountType: 'Saving Account',
+      balance: 20000,
+    }
+    const wrapper = shallow(
+      <AccountContainer
+        title="From Account"
+        onAccountSelect={onAccountSelect}
+        accounts={accounts}
+        selectedAccount={selectedAccount}
+        isFromAccount={false}
+        transferAmount={2000}
+      />
+    )
+    wrapper.find('Select').simulate('change', { key: 'Change', target: { value: 'mock' } })
+    expect(onAccountSelect).toHaveBeenCalled()
+    expect(onAccountSelect).toHaveBeenCalledWith('mock')
+  })
 })
